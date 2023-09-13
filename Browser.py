85560bda4188d647f5b4503416bc1e9697f8b52f
Sasibhusana behera <114127188+Sasi-2005@users.noreply.github.com>
2023-09-13 08:43:03 +0530

    Add files via upload

---
+++ Browser.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWebEngineWidgets import *
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow,self).__init__()
+        self.browser=QWebEngineView()
+        self.browser.setUrl(QUrl("http://facebook.com"))
+        self.setCentralWidget(self.browser)
+        self.showMaximized()
+
+app=QApplication(sys.argv)
+QApplication.setApplicationName("My Crazy Browser")
+window=MainWindow()
+app.exec_()
